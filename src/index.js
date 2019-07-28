a493935a20c07ea35afc492bbf9d22b382969efd
Aaron Saray <me@aaronsaray.com>
2019-07-27 20:07:22 -0500

    process

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 require("./styles/index.scss");
+
+window.addEventListener("scroll", e => {
+  let scrollDown = document.getElementById("scroll-down");
+  if (window.scrollY === 0) {
+    scrollDown.style.display = "block";
+  } else {
+    scrollDown.style.display = "none";
+  }
+});
