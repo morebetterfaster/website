24d834475bfcac8e9d78a72423c8d41512a93de5
Aaron Saray <me@aaronsaray.com>
2023-02-26 12:07:06 -0600

    Initial bootstrap setup.

---
+++ content/_index.md
@@ -0,0 +1,6 @@
+---
+title: "Elevating Laravel and PHP Projects | More Better Faster"
+date: 2023-02-26T11:49:40-06:00
+draft: false
+---
+
---
+++ themes/mbf/assets/main.js
@@ -0,0 +1,2 @@
+import Collapse from './themes/mbf/node_modules/bootstrap/js/dist/collapse.js'; // eslint-disable-line
+import Dropdown from './themes/mbf/node_modules/bootstrap/js/dist/dropdown.js'; // eslint-disable-line
